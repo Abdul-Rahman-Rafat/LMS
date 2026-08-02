273cd9622333d1fd1ea9fe4b146cf1c26dae50cf
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-08-02 21:08:31 +0300

    add project libraries and fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-5xl">
+      App أولا لمّا كان الاعتراف بالكرامة المتأصلة في جميع
+    </div>
+  );
 };
 
 export default App;
