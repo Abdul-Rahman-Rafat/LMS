49e474bd08cdfaeaa777d1cfef3f3f7547f1dba2
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-08-03 22:33:28 +0300

    add Hero + companies

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const Companies = () => {
-  return <div>Companies</div>;
+  return (
+    <div className="flex items-center justify-center gap-4 flex-wrap py-10 bg-cyan-100/70">
+      <img src={assets.microsoft_logo} alt="" />
+      <img src={assets.microsoft_logo} alt="" />
+      <img src={assets.microsoft_logo} alt="" />
+      <img src={assets.microsoft_logo} alt="" />
+      <img src={assets.microsoft_logo} alt="" />
+      <img src={assets.microsoft_logo} alt="" />
+    </div>
+  );
 };
 
 export default Companies;
--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import SearchBar from "../../components/student/SearchBar";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-10 bg-cyan-100/70">
+      <h1 className="text-3xl font-bold text-gray-800">
+        Welcome to Our Platform
+      </h1>
+
+      <img className="w-full max-w-2xl" src={assets.hero_image} alt="Hero" />
+      <p className="text-gray-600 text-center">
+        Discover a world of knowledge and unlock your potential with our
+        comprehensive learning platform.
+      </p>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Hero;
