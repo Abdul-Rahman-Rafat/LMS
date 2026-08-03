9ebbbbc8ab8662cd61fdc2ee3df35d950a31bffe
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-08-03 22:33:07 +0300

    add app context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,9 @@
 import { createContext } from "react";
 export const AppContext = createContext();
 
+const currency = import.meta.env.VITE_CURRENCY || "$";
+const value = { currency };
+
 export const AppContextProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
