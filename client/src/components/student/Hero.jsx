1a50d8f4c8d1297cf6ae9623acd8b1d69a76af63
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-08-04 04:43:13 +0300

    add hero banner image

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -4,17 +4,20 @@ import SearchBar from "../../components/student/SearchBar";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-10 bg-cyan-100/70">
-      <h1 className="text-3xl font-bold text-gray-800">
-        Welcome to Our Platform
-      </h1>
+    <div className="relative flex flex-col items-center justify-center gap-4  bg-cyan-100/70 ">
+      <img className="w-full " src={assets.heroBanner} alt="Hero" />
 
-      <img className="w-full max-w-2xl" src={assets.hero_image} alt="Hero" />
-      <p className="text-gray-600 text-center">
-        Discover a world of knowledge and unlock your potential with our
-        comprehensive learning platform.
-      </p>
-      <SearchBar />
+      <div className="absolute top-1/3 left-1/4 transform -translate-x-1/2 -translate-y-1/2">
+        <h1 className=" text-3xl font-bold text-gray-800">
+          Welcome to Our Platform
+        </h1>
+
+        <p className=" text-gray-600 text-center">
+          Discover a world of knowledge and unlock your potential with our
+          comprehensive learning platform.
+        </p>
+        <SearchBar />
+      </div>
     </div>
   );
 };
