1e103cab6cf2c31ee92b96837f7903ed0e325f6e
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-08-06 02:41:57 +0300

    add Rating for course

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Rating = () => {
-  return <div>Rating</div>;
+const Rating = (intialRating, onRate) => {
+  const [rating, setRating] = useState(intialRating || 0);
+  const handleRating = (value) => {
+    setRating(value);
+    if (onRate) onRate(value);
+  };
+  useEffect(() => {
+    if (intialRating) setRating(intialRating);
+  }, [intialRating]);
+  return (
+    <div>
+      {Array.from({ length: 5 }, (_, index) => {
+        const starValue = index + 1;
+        return (
+          <span
+            onClick={() => handleRating(starValue)}
+            key={index}
+            className={`text-x1 sm:text-2x1 cursor-pointer transition-colors ${starValue <= rating ? "text-yellow-500" : "text-gray-400"}`}
+          >
+            <span key={index}>★</span>
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Rating;
